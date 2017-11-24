85288b7af4278fe10edff6b72b9801035bddb37d
lukefang <lukeafang@gmail.com>
2017-11-24 18:41:48 -0500

    update mapgenerator
    add load map data from file

--- src/pathSchedule/MapGenerator.java
+++ src/pathSchedule/MapGenerator.java
@@ -1,5 +1,10 @@
 package pathSchedule;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MapGenerator
 {
 	public MapGenerator()
@@ -23,85 +28,7 @@ public class MapGenerator
 	public boolean generateMap_2(Map map)
 	{
 		map.clearNodes();
-		//start add nodes
-		String label = "";
-		int x = 0, y = 0;
-		int index = 0;
-		
-		label = String.format("%d", index++);
-		x = 1; y = 0;
-		map.addNode(label, x, y);			
-		
-		label = String.format("%d", index++);
-		x = 0; y = 2;
-		map.addNode(label, x, y);			
-		label = String.format("%d", index++);
-		x = 0; y = 3;
-		map.addNode(label, x, y);			
-		label = String.format("%d", index++);
-		x = 0; y = 4;
-		map.addNode(label, x, y);		
-		label = String.format("%d", index++);
-		x = 2; y = 2;
-		map.addNode(label, x, y);	
-		label = String.format("%d", index++);
-		x = 2; y = 3;
-		map.addNode(label, x, y);			
-		label = String.format("%d", index++);
-		x = 2; y = 4;
-		map.addNode(label, x, y);	
-		label = String.format("%d", index++);
-		x = 2; y = 5;
-		map.addNode(label, x, y);	
-		label = String.format("%d", index++);
-		x = 4; y = 5;
-		map.addNode(label, x, y);		
-		label = String.format("%d", index++);
-		x = 6; y = 5;
-		map.addNode(label, x, y);
-		label = String.format("%d", index++);
-		x = 8; y = 5;
-		map.addNode(label, x, y);
-		label = String.format("%d", index++);
-		x = 10; y = 5;
-		map.addNode(label, x, y);
-		
-		label = String.format("%d", index++);
-		x = 2; y = 6;
-		map.addNode(label, x, y);			
-		label = String.format("%d", index++);
-		x = 4; y = 6;
-		map.addNode(label, x, y);			
-		label = String.format("%d", index++);
-		x = 10; y = 6;
-		map.addNode(label, x, y);			
-		label = String.format("%d", index++);
-		x = 10; y = 8;
-		map.addNode(label, x, y);	
-		
-		label = String.format("%d", index++);
-		x = 4; y = 8;
-		map.addNode(label, x, y);	
-		label = String.format("%d", index++);
-		x = 4; y = 10;
-		map.addNode(label, x, y);	
-		label = String.format("%d", index++);
-		x = 0; y = 10;
-		map.addNode(label, x, y);		
-		label = String.format("%d", index++);
-		x = 10; y = 10;
-		map.addNode(label, x, y);	
-		
-		map.createParameterMatrices();//create edge matrices		
-
-		
-		return true;
-	}
-	
-	public boolean generateMap_1(Map map)
-	{
-		map.clearNodes();
-		
+		map.getPathNodeIndexList().clear();
 		//start add nodes
 		String label = "";
 		int x = 0, y = 0;
@@ -135,7 +62,7 @@ public class MapGenerator
 		label = String.format("%d", index++);
 		x = 2;
 		y = 0;
-		map.addNode(label, x, y);	
+		map.addNode(label, x, y);		
 		
 		map.createParameterMatrices();//create edge matrices		
 		
@@ -148,7 +75,77 @@ public class MapGenerator
 		if( map.addEdge("4", "2", 13, 1, false) == false ) { System.out.println("add edge fault"); }		
 		if( map.addEdge("4", "1", 4, 1, false) == false ) { System.out.println("add edge fault"); }		
 		if( map.addEdge("5", "4", 3, 1, false) == false ) { System.out.println("add edge fault"); }		
-		if( map.addEdge("5", "1", 9, 2, false) == false ) { System.out.println("add edge fault"); }		
+		if( map.addEdge("5", "1", 9, 2, false) == false ) { System.out.println("add edge fault"); }				
+		return true;
+	}
+	
+	public boolean generateMap_1(Map map)
+	{
+		map.clearNodes();
+		map.getPathNodeIndexList().clear();
+		
+		int index = 1;
+		
+		//load node information file and add node to map
+		String fileName = "";
+		fileName = String.format("mapData/%d_node.txt", index);	
+        try {
+            File f = new File(fileName);
+            BufferedReader b = new BufferedReader(new FileReader(f));
+
+            String readLine = "";
+            while ((readLine = b.readLine()) != null) {
+                //get label, x , y
+                String[] stringArray = readLine.split(" ");
+                String label = "";
+                int x = 0, y = 0;
+                label = stringArray[0];
+                x = Integer.parseInt(stringArray[1]);
+                y = Integer.parseInt(stringArray[2]);
+                map.addNode(label, x, y);	       
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        map.createParameterMatrices();//create edge matrices	
+		
+      //load edge information file and add edge to map
+		fileName = String.format("mapData/%d_edge.txt", index);	
+        try {
+            File f = new File(fileName);
+            BufferedReader b = new BufferedReader(new FileReader(f));
+
+            String readLine = "";
+            while ((readLine = b.readLine()) != null) {
+            	//get edge information
+            	String[] stringArray = readLine.split(" ");
+            	String label_1 = "", label_2 = "";
+            	int dis = 0;
+            	double energyCost = 0.0f;
+            	boolean rechargeStation = false;
+            	int tempD = 0;
+            	
+            	label_1 = stringArray[0];
+            	label_2 = stringArray[1];
+            	dis = Integer.parseInt(stringArray[2]);
+            	energyCost = Double.parseDouble(stringArray[3]);
+            	tempD = Integer.parseInt(stringArray[4]);
+            	if(tempD == 1)	{ rechargeStation = true; }
+            	else			{ rechargeStation = false; }
+            	
+            	if( map.addEdge(label_1, label_2, dis, energyCost, rechargeStation) == false ) 
+            	{ 
+            		System.out.println("add edge fault ("+label_1 + ", " + label_2+")");
+            	}		
+ 	
+            	
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }        
+	  
+       
 			
 		return true;
 	}
